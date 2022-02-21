f8fc5039b0f01b6ea4c9d9f313a2e394f9b7ecd0
MayaAusset <mayaausset@gmail.com>
2022-02-21 12:14:13 +1100

    event page

--- src/components/pages/event/Event.js
+++ src/components/pages/event/Event.js
@@ -9,30 +9,42 @@ const Event = () => {
 
   return (
     <div className={styles.wrapper}>
-      <img src="" alt="event banner" />
-      <h1>🎉 {event.name}e</h1>
+      <img src={'/images/event_cover_photo.png'} alt="event banner" className={styles.banner_image} />
+      <div className={styles.title}>
+        <img src={'/images/party.png'} alt="party emoji" className={styles.partyemoji} />
+        <h1> {event.name}</h1>
+      </div>
       <h2>Hosted by {event.hostName}</h2>
       <div className={styles.attendance}>
-        <img src="" alt="event banner" />
-        <img src="" alt="event banner" />
-        <img src="" alt="event banner" />
+        <img src="/images/going_button.png" alt="going button" />
+        <img src="/images/maybe_button.png" alt="maybe button" />
+        <img src="/images/not_going_button.png" alt="not going button" />
       </div>
       <div className={styles.invitation_settings}>
-        <button>✉️ Invite a friend</button>
-        <button>...</button>
+        <img src="/images/invite_button.png" alt="invite button" className={styles.invite_btn} />
+        <img src="/images/settings_button.png" alt="settings button" className={styles.settings_btn} />
       </div>
       <div className={styles.dates}>
-        <img/>
+        <img src={'/images/pin.png'} alt="pin emoji" className={styles.emoji} />
         <div>
           <p>{event.startDate}</p>
           <p>to {event.endDate}</p>
-          <p className={styles.addToCal}>Add to calendar</p>
+          <p className={styles.subText}>Add to calendar</p>
         </div>
       </div>
-      <div className={styles.location}></div>
-      <div className={styles.details}></div>
+      <div className={styles.location}>
+        <img src={'/images/calendar.png'} alt="calendar emoji" className={styles.emoji} />
+        <div>
+          <p>{event.location}</p>
+          <p className={styles.subText}>Open in maps</p>
+        </div>
+      </div>
+      <div className={styles.details}>
+        <img src="/images/details_button.png" alt="details button" className={styles.detais_btn} />
+        <img src="/images/posts_button.png" alt="posts button" className={styles.posts} />
+      </div>
       <div>
-        <p>DESCRIPTION HERE</p>
+        <p className={styles.subText_description}>Just a lowkey dinner to celebrate my 22nd. Would love to see you </p>
       </div>
     </div>
   );
